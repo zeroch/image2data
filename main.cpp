c26dc9707a0d1682dc19399e4683a966427f9815
zero <chenze62@gmail.com>
2014-04-20 17:17:37 -0400

    convert to .h file.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <QDebug>
 #include <QImage>
 #include <iostream>
-
+#include <QRgb>
+#include <QString>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -18,15 +19,23 @@ int main(int argc, char *argv[])
         int row,col;
         row = my_image->size().height();
         col = my_image->size().width();
-
+        QString rgb_value;
+        QStringRef ref_value;
+        std::cout << "const int img[] = { \n";
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 count++;
-                std::cout << my_image->pixel(i,j) << "\t";
+                int value = my_image->pixel(i,j);
+                rgb_value = QString::number(value, 16);
+                ref_value= rgb_value.rightRef(6);
+                rgb_value = "0x"+ref_value.toString();
+                std::cout << rgb_value.toStdString() << ", ";
+//                qDebug()<< rgb_value <<",";
             }
             std::cout << "\n";
         }
-        qDebug() << "[debug]: total pixel " << count ;
+        std::cout <<"};";
+        //qDebug() << "[debug]: total pixel " << count ;
 
     }
 
